434d6ee16c9a35af2e328c9dc0113f78aa436e67
Thays Lira <lirazootech@outlook.com>
2025-03-23 19:44:15 +0000

    Melhoria do código durante a realização do desafio

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -2,26 +2,60 @@
 
 # Vamos lá!
 
+while True:
+
 # Primeiro, vamos solicitar o primeiro número do usuário
 
-num1 = float(input("Digite o primeiro número: "))
+    num1 = float(input("Digite o primeiro número: "))
 
 # Agora, vamos solicitar o segundo número do usuário
 
-num2 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o primeiro número: "))
 
-# Agora, vamos realizar a operação entre os números
+# Vamos perguntar ao usuário qual operação ele deseja realizar
 
-soma = num1 + num2
-subtracao = num1 - num2
-multiplicacao = num1 * num2
-divisao = num1 / num2
-potencia = num1 ** num2
+    operacao = input("Qual operação você deseja realizar (+, -, *, /, ^, todas)?. Ou digite 'sair' para encerrar: ")
 
-# Por fim, vamos exibir os resultados das operações
-print("Resultados das operações:")
-print(f"Soma: {num1} + {num2} = {soma}")
-print(f"Subtração: {num1} - {num2} = {subtracao}")
-print(f"Multiplicação: {num1} * {num2} = {multiplicacao}")
-print(f"Divisão: {num1} / {num2} = {divisao}")
-print(f"Potência: {num1} ** {num2} = {potencia}")
+# Verifica se o usuário deseja encerrar o programa
+
+    if operacao.lower() == "sair":
+        print("Encerrando o programa. Até mais!")
+    break
+
+# Agora, vamos realizar a operação solicitada
+
+if operacao == "+":
+    print(f"Resultado da soma: {num1} + {num2} = {num1 + num2}")
+
+elif operacao == "-":
+    print(f"Resultado da subtração: {num1} - {num2} = {num1 - num2}")
+
+elif operacao == "*":
+    print(f"Resultado da multiplicação: {num1} * {num2} = {num1 * num2}")
+
+elif operacao == "/":
+    if num2 == 0:
+        print("Não é possível dividir por zero!")
+    else:
+        print(f"Resultado da divisão: {num1} / {num2} = {num1 / num2}")
+
+elif operacao == "^":
+    print(f"Resultado da potenciação: {num1} ** {num2} = {num1 ** num2}")
+
+elif operacao.lower() == "todas":
+    print("Resultados das operações:")
+    print(f"Soma: {num1} + {num2} = {num1 + num2}")
+    print(f"Subtração: {num1} - {num2} = {num1 - num2}")
+    print(f"Multiplicação: {num1} * {num2} = {num1 * num2}")
+    
+    if num2 != 0:     
+        print(f"Divisão: {num1} / {num2} = {num1 / num2}")
+    else:
+        print("Não é possível dividir por zero!")
+
+    print(f"Potência: {num1} ** {num2} = {num1 ** num2}")
+else:
+    print("Operação inválida. Tente novamente!")
+
+# Linha em branco para separar as operações
+print()
