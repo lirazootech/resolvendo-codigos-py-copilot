d10c422cf420ad5f4e70b74fa048e90558b00316
Thays Lira <lirazootech@outlook.com>
2025-03-24 19:52:37 +0000

    Add resolução dos desafios extras

---
+++ media_notas.py
@@ -0,0 +1,27 @@
+# Vamos calcular a média de três notas informadas pelo usuário.
+
+# Vamos lá!
+
+# Primeiro, vamos receber as notas do usuário
+
+nota1 = float(input("Digite a primeira nota, um valor de 0 a 10: "))
+nota2 = float(input("Digite a segunda nota, um valor de 0 a 10: "))
+nota3 = float(input("Digite a terceira nota, um valor de 0 a 10: "))
+
+# Agora, vamos calcular a média das notas
+
+media = (nota1 + nota2 + nota3) / 3
+
+# Por fim, vamos exibir a média das notas
+
+print("A média das notas é: ", media)
+
+if media >= 7:
+    print("Aprovado!")
+elif media >= 5:
+    print("Recuperação!")
+else:
+    print("Reprovado!")
+
+# Linha em branco para separar as operações
+print()
--- resolucoes_code/par_ou_impar.py
+++ resolucoes_code/par_ou_impar.py
@@ -1,5 +1,18 @@
-# Como entrada receber um número inteiro para saber se ele é par ou ímpar
+# Vamos receber como entrada um número inteiro para saber se ele é par, ímpar ou neutro.
 
-num1 = int(input("Digite o número desejado para saber se ele é par ou ímpar: "))
+# Vamos lá!
 
-#
+num1 = int(input("Digite o número inteiro para saber se ele é par ou ímpar: "))
+
+# Condição para definir se o número é par ou ímpar
+
+if num1 == 0:
+    print("O número 0 é neutro.")
+elif num1 % 2 == 0:
+    print(f"O número {num1} é par.")   
+else:
+    print(f"O número {num1} é ímpar.")
+
+# Linha em branco para separar as operações
+
+print() 
